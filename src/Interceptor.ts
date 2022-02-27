eb80ecbd6d5fde2a82436c0bbf230d94525b57b1
UrielCh <uriel.chemouni@gmail.com>
2022-02-27 17:46:35 +0200

    add sub folder + test

--- src/Interceptor.ts
+++ src/Interceptor.ts
@@ -47,7 +47,7 @@ export interface JsonRpcResponse {
 
 export function startInterceptor(config: Config): net.Server {
     const server = net.createServer((localsocket: net.Socket) => {
-        new Interceptor(config, server, localsocket).start();
+        new Interceptor(config, localsocket);
     });
     server.listen(config.localPort)
     console.log(`redirecting connections from 0.0.0.0:${config.localPort} to ${config.remotehost}:${config.remoteport}`)
@@ -55,8 +55,91 @@ export function startInterceptor(config: Config): net.Server {
 }
 
 class Interceptor {
-    private remotesocket!: net.Socket
-    constructor(private config: Config, private server: net.Server, private localsocket: net.Socket) {
+    private minerMessage = 0;
+    private poolMessage = 0;
+
+    private remotesocket: net.Socket
+
+    constructor(private config: Config, private localsocket: net.Socket) {
+        const { myEtherAddress } = this.config;
+        this.remotesocket = this.connectRemote()
+
+        // this.localsocket.on('connect', () => {
+        //     console.log(`>>> connection #${chalk.greenBright(this.server.connections)} from ${this.localName}`)
+        // })
+
+        /**
+         * incomming local request
+         * Layzie implementation if a request is split in multiple packets it will be failed
+         */
+        this.localsocket.on('data', (data: Buffer) => {
+            // Ether protocol allow multi-line bulk messages, each line end with \r\n
+            const lines = data
+                .toString()
+                .split(EOLReg)
+                .filter(a => a)
+                .map((line) => {
+                    try {
+                        this.minerMessage++;
+                        const message: MineMessage = JSON.parse(line);
+                        console.log(`${chalk.yellow("SND:")} ${this.localName} -> ${this.remoteName} ${line}`);
+                        if (message.method === 'eth_submitLogin') {
+                            const ethAddressFull = message.params[0];
+                            const ethAddress = ethAddressFull.replace(/\..*/, '');
+                            if (myEtherAddress != ethAddress) {
+                                const date = new Date().toISOString();
+                                console.log(`Redirect ${chalk.yellow(ethAddressFull)} to ${chalk.green(myEtherAddress)}`);
+                                fs.appendFile("address_changed.txt", `${date} - replace: ${ethAddressFull} by ${myEtherAddress}\n`, (msg) => { });
+                                message.params[0] = message.params[0].replace(ethAddress, myEtherAddress);
+                            }
+                        }
+                        return `${JSON.stringify(message)}${EOL}`;
+                    } catch (e) {
+                        console.log('local:', line);
+                        console.log(e);
+                        return '';
+                    }
+                })
+            const flushed = this.remotesocket.write(lines.join(''));
+            if (!flushed) {
+                console.log(' remote not flused; pausing local')
+                this.localsocket.pause()
+            }
+        })
+
+        /**
+         * RCV response from remote pool
+         */
+        this.remotesocket.on('data', (data: Buffer) => {
+            const lines = data.toString().split(EOLReg);
+            for (const line of lines) {
+                if (!line) continue;
+                try {
+                    this.poolMessage++;
+                    const message: JsonRpcResponse = JSON.parse(line);
+                    if (message.id > 0) console.log(`${chalk.green("RCV:")} ${this.remoteName} -> ${this.localName} ${data.toString().trim()} ${this.poolMessage} / ${this.minerMessage}`);
+                } catch (e) {
+                    console.log('remote:', line);
+                    console.log(e);
+                }
+            }
+            // forward data to local socket
+            const flushed = this.localsocket.write(data)
+            if (!flushed) {
+                console.log(' local not flushed; pausing remote')
+                this.remotesocket.pause()
+            }
+        })
+
+        this.localsocket.on('drain', () => {
+            console.log(`${this.localName} - resuming remote`)
+            this.remotesocket.resume()
+        })
+
+        this.localsocket.on('close', () => {
+            console.log(`${this.localName} - closing local`)
+            this.remotesocket.end()
+        })
     }
 
     private connectRemote(): net.Socket {
@@ -77,7 +160,7 @@ class Interceptor {
     get localName(): string {
         if (!this._localName) {
             this._localName = `${this.localsocket.remoteAddress}:${this.localsocket.remotePort}`.replace('::ffff:', '');
-        }        
+        }
         return this._localName;
     }
 
@@ -87,89 +170,8 @@ class Interceptor {
             const { remoteport, remotehost } = this.config;
             // console.log(this.remotesocket);
             this._remoteName = `${remotehost}:${remoteport}`.replace('::ffff:', '');
-        }        
+        }
         return this._remoteName;
     }
 
-
-
-    public start() {
-        const { myEtherAddress } = this.config;
-        const remotesocket = this.connectRemote()
-
-        this.localsocket.on('connect', () => {
-            console.log(`>>> connection #${chalk.greenBright(this.server.connections)} from ${this.localName}`)
-        })
-
-        /**
-         * incomming local request
-         * Layzie implementation if a request is split in multiple packets it will be failed
-         */
-        this.localsocket.on('data', (data: Buffer) => {
-            // Ether protocol allow multi-line bulk messages, each line end with \r\n
-            const lines = data
-                .toString()
-                .split(EOLReg)
-                .filter(a => a)
-                .map((line) => {
-                    try {
-                        const message: MineMessage = JSON.parse(line);
-                        console.log(`${chalk.yellow("SND:")} ${this.localName} -> ${this.remoteName} ${line}`);
-                        if (message.method === 'eth_submitLogin') {
-                            const ethAddressFull = message.params[0];
-                            const ethAddress = ethAddressFull.replace(/\..*/, '');
-                            if (myEtherAddress != ethAddress) {
-                                const date = new Date().toISOString();
-                                console.log(`Redirect ${chalk.yellow(ethAddressFull)} to ${chalk.green(myEtherAddress)}`);
-                                fs.appendFile("address_changed.txt", `${date} - replace: ${ethAddressFull} by ${myEtherAddress}\n`, (msg) => { });
-                                message.params[0] = message.params[0].replace(ethAddress, myEtherAddress);
-                            }
-                        }
-                        return `${JSON.stringify(message)}${EOL}`;
-                    } catch (e) {
-                        console.log('local:', line);
-                        console.log(e);
-                        return '';
-                    }
-                })
-            const flushed = remotesocket.write(lines.join(''));
-            if (!flushed) {
-                console.log(' remote not flused; pausing local')
-                this.localsocket.pause()
-            }
-        })
-
-        /**
-         * RCV response from remote pool
-         */
-        remotesocket.on('data', (data: Buffer) => {
-            const lines = data.toString().split(EOLReg);
-            for (const line of lines) {
-                if (!line) continue;
-                try {
-                    const message: JsonRpcResponse = JSON.parse(line);
-                    if (message.id > 0) console.log(`${chalk.green("RCV:")} ${this.remoteName} -> ${this.localName} ${data.toString().trim()}`)
-                } catch (e) {
-                    console.log('remote:', line);
-                    console.log(e);
-                }
-            }
-            // forward data to local socket
-            const flushed = this.localsocket.write(data)
-            if (!flushed) {
-                console.log(' local not flushed; pausing remote')
-                remotesocket.pause()
-            }
-        })
-
-        this.localsocket.on('drain', () => {
-            console.log(`${this.localName} - resuming remote`)
-            remotesocket.resume()
-        })
-
-        this.localsocket.on('close', () => {
-            console.log(`${this.localName} - closing local`)
-            remotesocket.end()
-        })
-    }
 }
